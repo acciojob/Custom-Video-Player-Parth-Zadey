23b3cf095e4a6de7d9173bf7d156c074105dfabe
Parth-Zadey <parthzadey35@gmail.com>
2026-09-21 17:49:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,8 +34,10 @@ function handleRangeUpdate() {
 
 // Update progress bar
 function handleProgress() {
+  if (!video.duration) return;
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
+  progressBar.style.width = `${percent}%`;
 }
 
 // Scrub video by clicking/dragging on the progress bar
